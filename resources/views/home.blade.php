6c8bf7e619e4e38422a024007a16ae93bc68afb0
Edgar Hdez <edgaarhdez96@gmail.com>
2026-03-30 14:59:46 -0600

    feat: Integración AdminLTE con auth scaffolding

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Bienvenido al panel</h3>
+                </div>
+                <div class="card-body">
+                    <p>¡Proyecto inicializado correctamente!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
